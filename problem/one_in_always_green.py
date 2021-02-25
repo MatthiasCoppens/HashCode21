a25f4b57ba6384e0ece8ba3413c49d7d67d92cbf
Sibe Bleuzé <sibebleuze@gmail.com>
2021-02-25 20:22:12 +0100

    put the traffic lights that should always be green in one list and write some code to parse this to the expected output

--- problem/one_in_always_green.py
+++ problem/one_in_always_green.py
@@ -1,8 +1,16 @@
+# for putting the intersections with one street incoming in a dict
 intersections = {0: [["straat_1"], ["straat_2"]]}
-f = open("output_file", "w")
-for intersection in intersections.keys():
+traffic_lights = {}
+for intersection in intersections:
     if len(intersections[intersection][0]) == 1:
-        print(intersection, file=f)
-        print(1, file=f)
-        print(intersections[intersection][0][0], 1, file=f)
+        # traffic_lights[intersection] = list of tuples with street names and time per cycle
+        traffic_lights[intersection] = [(intersections[intersection][0][0], 1)]
+
+# for printing the traffic light from the format above
+f = open("output_file", "w")
+for intersection in traffic_lights:
+    print(intersection, file=f)
+    print(len(traffic_lights[intersection]), file=f)
+    for streetname, time in traffic_lights[intersection]:
+        print(streetname, time, file=f)
 f.close()
