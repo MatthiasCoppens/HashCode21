4182a282bb21050d7a7e60b0f7cc16fc81751abe
epsegers <emile.segers8@gmail.com>
2021-02-25 20:25:59 +0100

    data reading function

--- heatmap.py
+++ heatmap.py
@@ -1,54 +1,68 @@
-filename = "./problem/input_A"
+def read_sim_data(filename):
+    """
+    input:
+        name of file
 
-"""
-key: streetname
-data:
-    - start intersection
-    - stop intersection
-    - time to travel
-    - amount of passing cars
-"""
-streets = {}
-
-"""
-key: intersection ID
-data:
-    - incomming streets
-    - outgoing streets
-    - amount of passing cars
-"""
-intersections = {}
-vehicles = []
-
-with open(filename) as file:
-    # read in simulation data
-    d,i,s,v,f = [int(i) for i in file.readline().rstrip().split(" ")]
+    output:
+        - tuple with simulation data
+        - dictionary with street data
+        - dictionary with intersection data
+        - list with vehicle routes
+    """
     
-    # read in street and intersection data
-    for _ in range(s):
-        line = file.readline().rstrip().split(" ")
+    """
+    key: streetname
+    data:
+        - start intersection
+        - stop intersection
+        - time to travel
+        - amount of passing cars
+    """
+    streets = {}
+    
+    """
+    key: intersection ID
+    data:
+        - incomming streets
+        - outgoing streets
+        - amount of passing cars
+    """
+    intersections = {}
 
-        streets[line[2]] = [int(line[0]), int(line[1]), int(line[3]), 0]
+    """
+    list of vehicle paths
+    """
+    vehicles = []
+    
+    with open(filename) as file:
+        # read in simulation data
+        d,i,s,v,f = [int(i) for i in file.readline().rstrip().split(" ")]
         
-        if int(line[0]) not in intersections:
-            intersections[int(line[0])] = [[], [line[2]], 0]
-        else:
-            intersections[int(line[0])][1].append(line[2])
+        # read in street and intersection data
+        for _ in range(s):
+            line = file.readline().rstrip().split(" ")
+    
+            streets[line[2]] = [int(line[0]), int(line[1]), int(line[3]), 0]
+            
+            if int(line[0]) not in intersections:
+                intersections[int(line[0])] = [[], [line[2]], 0]
+            else:
+                intersections[int(line[0])][1].append(line[2])
+    
+            if int(line[1]) not in intersections:
+                intersections[int(line[1])] = [[line[2]], [], 0]
+            else:
+                intersections[int(line[1])][0].append(line[2])
+    
+        # read in vehicles
+        for _ in range(v):
+            line = file.readline().rstrip().split(" ")
+    
+            vehicles.append(line[1:])
+            for street in vehicles[-1][:-1]:
+                streets[street][-1]+=1
+                intersections[streets[street][1]][-1]+=1
 
-        if int(line[1]) not in intersections:
-            intersections[int(line[1])] = [[line[2]], [], 0]
-        else:
-            intersections[int(line[1])][0].append(line[2])
+    return (d,i,s,v,f), streets, intersections, vehicles
 
-    # read in vehicles
-    for _ in range(v):
-        line = file.readline().rstrip().split(" ")
-
-        vehicles.append(line[1:])
-        for street in vehicles[-1][:-1]:
-            streets[street][-1]+=1
-            intersections[streets[street][1]][-1]+=1
-
-
-
-print(intersections)
+print(read_sim_data("./problem/input_A"))
