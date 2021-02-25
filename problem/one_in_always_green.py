113a9615cbac29b2a4f9d9c54d6de71e7faf5333
Sibe Bleuzé <sibebleuze@gmail.com>
2021-02-25 20:03:08 +0100

    print to file in one_in_always_green

--- problem/one_in_always_green.py
+++ problem/one_in_always_green.py
@@ -1,7 +1,8 @@
-intersections = {0: [[], []]}
+intersections = {0: [["straat_1"], ["straat_2"]]}
 f = open("output_file", "w")
 for intersection in intersections.keys():
     if len(intersections[intersection][0]) == 1:
-        f.write(intersection)
-        f.write(1)
-        f.write(intersections[intersection][0][0], 1)
+        print(intersection, file=f)
+        print(1, file=f)
+        print(intersections[intersection][0][0], 1, file=f)
+f.close()
