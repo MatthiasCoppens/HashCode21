997d3b09a0e7a61d37fdd30836fd5ed65b53213f
NielsVdBossche <niels.vandenbossche@ugent.be>
2021-02-25 20:33:14 +0100

    lights weighted

---
+++ problem/weighted_lights.py
@@ -0,0 +1,95 @@
+for c in 'ABCDEF':
+
+    filename = f'input_{c}'
+
+    """
+    key: streetname
+    data:
+        - start intersection
+        - stop intersection
+        - time to travel
+        - amount of passing cars
+    """
+    streets = {}
+
+    """
+    key: intersection ID
+    data:
+        - incomming streets
+        - outgoing streets
+        - amount of passing cars
+    """
+    intersections = {}
+    vehicles = []
+
+    with open(filename) as file:
+        # read in simulation data
+        d, i, s, v, f = [int(i) for i in file.readline().rstrip().split(" ")]
+
+        # read in street and intersection data
+        for _ in range(s):
+            line = file.readline().rstrip().split(" ")
+
+            streets[line[2]] = [int(line[0]), int(line[1]), int(line[3]), 0]
+
+            if int(line[0]) not in intersections:
+                intersections[int(line[0])] = [[], [line[2]], 0]
+            else:
+                intersections[int(line[0])][1].append(line[2])
+
+            if int(line[1]) not in intersections:
+                intersections[int(line[1])] = [[line[2]], [], 0]
+            else:
+                intersections[int(line[1])][0].append(line[2])
+
+        # read in vehicles
+        for _ in range(v):
+            line = file.readline().rstrip().split(" ")
+
+            vehicles.append(line[1:])
+            for street in vehicles[-1][:-1]:
+                streets[street][-1] += 1
+                intersections[streets[street][1]][-1] += 1
+
+
+    # initial state
+    # check which roads are busy, which not, how long
+    """
+    intersections = dict()
+    intersec_state = dict()
+    time = 0
+    
+    for intersection in intersections.keys():
+        cars_per_street = []
+        for street in intersections[intersection][0]:
+            cars_queued = 0
+            for car in vehicles.keys():
+                if vehicles[car][0] == street:
+                    cars_queued += 1
+            cars_per_street.append(cars_queued)
+    
+        longest_queue = cars_per_street.index(max(cars_per_street))
+        if cars_per_street[longest_queue] != 0:
+            intersec_state[intersection] = (longest_queue, cars_per_street[longest_queue] // len(cars_per_street))
+        else:
+            intersec_state[intersection] = (longest_queue, 1)
+    """
+
+    file_out = open(f'output_{c}', 'w')
+
+    print(len(intersections), file=file_out)
+    for intersection, inter_data in intersections.items():
+        print(intersection, file=file_out)
+        print(len(inter_data[0]), file=file_out)
+
+        for street in inter_data[0]:
+            if streets[street][3] < len(inter_data[1]):
+                time = 1
+            else:
+                time = streets[street][3] // len(inter_data[1])
+
+            print(f'{street} {time}', file=file_out)
+
+        # get corresponding intersection
+
+        # set schedule for the intersection as to
