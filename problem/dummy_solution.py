a35fe174467e827c8810e5750f8c85dba2c8f836
Matthias Coppens <coppens.matthias.abc@gmail.com>
2021-02-25 19:40:13 +0100

    for loop

--- problem/dummy_solution.py
+++ problem/dummy_solution.py
@@ -1,19 +1,21 @@
-file_in = open('input_F', 'r')
-file_out = open('output_F', 'w')
+for c in 'ABCDEF':
 
-inp = file_in.read().splitlines()
-l = int(inp[0].split()[0])
-lines = [l.split() for l in inp[1:l+1]]
+    file_in = open(f'input_{c}', 'r')
+    file_out = open(f'output_{c}', 'w')
 
-db = {}
+    inp = file_in.read().splitlines()
+    l = int(inp[0].split()[0])
+    lines = [l.split() for l in inp[1:l+1]]
 
-for line in lines:
-    db[line[1]] = db.get(line[1], [])
-    db[line[1]].append(line[2])
+    db = {}
 
-print(len(db), file=file_out)
-for intersection, streets in db.items():
-    print(intersection, file=file_out)
-    print(len(streets), file=file_out)
-    for street in streets:
-        print(f'{street} 1', file=file_out)
+    for line in lines:
+        db[line[1]] = db.get(line[1], [])
+        db[line[1]].append(line[2])
+
+    print(len(db), file=file_out)
+    for intersection, streets in db.items():
+        print(intersection, file=file_out)
+        print(len(streets), file=file_out)
+        for street in streets:
+            print(f'{street} 1', file=file_out)
